3f1a924ef44a748e96077945ce596817b76c8556
rob <collinsduo1@gmail.com>
2023-10-03 11:32:06 -0500

    fix server for reading mongoose

--- server.js
+++ server.js
@@ -7,16 +7,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-    // useFindAndModify: false,
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/RobbinNoSQLChallengeSocialNetworkAPI', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, err => {
-    if (err) throw err;
-    console.log('Connected to MongoDB!')
-});
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-// Use this to log mongo queries being executed
+// Use this to log MongoDB queries being executed
 mongoose.set('debug', true);
 
 app.use(require('./routes'));
